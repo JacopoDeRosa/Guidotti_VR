4bd443991380e76bbdaa790e44bb404d9976178c
Jacopo De Rosa <jacopo.derosa00@gmail.com>
2024-11-17 10:05:26 +0100

    Add Required Fixes

--- Assets/Scripts/UI/HighScoreUI.cs
+++ Assets/Scripts/UI/HighScoreUI.cs
@@ -10,6 +10,7 @@ namespace UI
     {
         [SerializeField] private HighScoreManager _highScoreManager;
         [SerializeField] private TMP_Text[] _highscoreListTexts;
+        [SerializeField] private TMP_Text _finalScoreText;
 
         private void OnValidate()
         {
@@ -37,12 +38,21 @@ namespace UI
             }
             
             List<PlayerScore> scores = _highScoreManager.GetTopScores(_highscoreListTexts.Length);
+
+            if (scores.Count == 0)
+            {
+                _highscoreListTexts[0].gameObject.SetActive(true);
+                _highscoreListTexts[0].text = "No high scores yet!";
+                return;
+            }
+            
             for (int i = 0; i < scores.Count; i++)
             {
                 _highscoreListTexts[i].gameObject.SetActive(true);
                 _highscoreListTexts[i].text = $"[{scores[i].playerName}] Glucose: {scores[i].glucoseLevel}% Sodium: {scores[i].sodiumLevel}%";
             }
             
+            if(_finalScoreText) _finalScoreText.text = $"[{_highScoreManager.CurrentScore.playerName}] Glucose: {_highScoreManager.CurrentScore.glucoseLevel}% Sodium: {_highScoreManager.CurrentScore.sodiumLevel}%";
         }
         
         private void OnFinalScoreUpdated()
