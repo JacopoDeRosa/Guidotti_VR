e89fafb6f2b4a9f8bfd8fb7c3e1786a895a7ce00
Jacopo De Rosa <jacopo.derosa00@gmail.com>
2025-01-03 15:54:35 +0100

    Add Video player

---
+++ Assets/Scripts/UI/IntroVideoPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace UI
+{
+    [RequireComponent(typeof(VideoPlayer))]
+    public class IntroVideoPlayer : MonoBehaviour
+    {
+        [SerializeField] private VideoPlayer _videoPlayer;
+        
+        private void OnValidate()
+        {
+            _videoPlayer = GetComponent<VideoPlayer>();
+        }
+
+        private void Start()
+        {
+            _videoPlayer.isLooping = false;
+            _videoPlayer.loopPointReached += OnVideoEnd;
+        }
+
+        private void OnVideoEnd(VideoPlayer source)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
